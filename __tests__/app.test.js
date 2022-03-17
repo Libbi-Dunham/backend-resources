62ef16776962ac15674c2bcd4399b85b8859c32f
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-17 09:42:27 -0700

    got first test to pass/insert for dogs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Dog = require('../lib/models/Dogs');
 
 describe('backend-resources routes', () => {
   beforeEach(() => {
@@ -10,5 +11,17 @@ describe('backend-resources routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create a dog', async () => {
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send({ name: 'miklo', quantity: 1 });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'miklo',
+      quantity: 1,
+    });
   });
 });
