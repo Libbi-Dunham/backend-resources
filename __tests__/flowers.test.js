baa1847e16426c4083074cc11107894109cc41bf
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-18 14:29:22 -0700

    got my second test to pass getById for flowers

--- __tests__/flowers.test.js
+++ __tests__/flowers.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Flower = require('../lib/models/Flowers');
 
 describe('backend-resources routes', () => {
   beforeEach(() => {
@@ -22,5 +23,11 @@ describe('backend-resources routes', () => {
       name: 'rose',
       color: 'pink',
     });
+  });
+
+  it('should list a flower by id', async () => {
+    const flower = await Flower.insert({ name: 'rose', color: 'pink' });
+    const res = await request(app).get(`/api/v1/flowers/${flower.id}`);
+    expect(res.body).toEqual(flower);
   });
 });
