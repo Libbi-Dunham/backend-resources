a3b3d9fb5b1725f959e2c50cbd08532405b82ac5
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-03-18 11:51:41 -0700

    got my first test to pass insert for movies and fixed an error I was having(made my describe async)

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -2,13 +2,26 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const Movie = require('../lib/models/Movies');
 
-describe('backend-resources routes', async () => {
+describe('backend-resources routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create a movie', async () => {
+    const res = await request(app)
+      .post('/api/v1/movies')
+      .send({ title: 'pulp fiction', director: 'quentin tarantino' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: 'pulp fiction',
+      director: 'quentin tarantino',
+    });
   });
 });
